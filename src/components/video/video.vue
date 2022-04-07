76aa12da9606d4b79320efb01bb910343f186561
YangChaoJie <1126913855@qq.com>
2022-04-07 19:04:51 +0800

    commit fix code

--- src/components/video/video.vue
+++ src/components/video/video.vue
@@ -10,13 +10,114 @@
       :autoplay="true"
       class="video-js vjs-big-play-centered"
     ></video>
-    <!-- <video-js id="vid1" width="600" height="300" class="vjs-default-skin" controls>
-      <source src="https://example.com/index.m3u8" type="application/x-mpegURL" />
-    </video-js>-->
   </div>
 </template>
 
-<script lang="js">
+<script setup lang="ts">
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+import './video.scss'
+import 'videojs-landscape-fullscreen'
+import './resolution-switcher/index.js'
+import { reactive, watchEffect, ref } from 'vue-demi'
+const DEFAULT_EVENTS = [
+  'loadstart',
+  'loadeddata',
+  'canplay',
+  'canplaythrough',
+  'play',
+  'timeupdate',
+  'pause',
+  'waiting',
+  'playing',
+  'ended',
+  'error'
+]
+
+const props = defineProps<{
+  options: object
+}>()
+
+const emit = defineEmits(DEFAULT_EVENTS)
+
+const defaultConfig = reactive({
+  controls: true,
+  autoplay: true,
+  playbackRate: [1, 1.5, 2],
+  muted: false,
+  poster: 'none',
+  plugins: {
+    videoJsResolutionSwitcher: {
+      default: 'high',
+      ui: true,
+      dynamicLabel: true
+    }
+  },
+  videoOptions: {}
+})
+
+let videoOptions = {};
+
+watchEffect(() => {
+  videoOptions = {
+    ...defaultConfig,
+    ...props.options
+  }
+})
+const videoPlayer = ref<HTMLElement | null>(null)
+let player: videojs.Player
+function initialize() {
+  const emitPlayerSate = (event: string) => {
+    if (event) {
+      switch (event) {
+        case 'loadstart':
+          // this.decodeHls()
+          break;
+        case 'error':
+          // this.onPlayError()
+          break;
+        default:
+          break;
+      }
+    }
+    emit(event)
+  }
+
+  player = videojs(videoPlayer.value ?? '', videoOptions, function onPlayReady() {
+    const events = DEFAULT_EVENTS
+    for (let i = 0; i < events.length; i++) {
+      if (typeof events[i] === 'string') {
+        (event => {
+          this.on(event, () => {
+            emitPlayerSate(event)
+          })
+        })(events[i])
+      }
+    }
+  })
+
+  videojs.use('video/mp4', (player) => {
+    console.log('正在播放');
+    return {
+      setSource(srcObj, next) {
+        next(null, srcObj)
+      },
+      setVolume(val: number) {
+        console.log('val----', val);
+        return 0.2
+      }
+    }
+  })
+}
+
+function newButtonToggle() {
+  // 隐藏掉 画中画
+  player?.getChild('ControlBar')?.removeChild(player?.getChild('pictureInPictureToggle')!)
+}
+
+</script>
+
+<!-- <script lang="ts">
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 import './video.scss'
@@ -192,4 +293,4 @@ export default {
 </script>
 
 <style>
-</style>
+</style> -->
